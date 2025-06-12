6dd026ea6b8ee2cccbee4e4eb0fa15f7418d8a52
Pranay A. <ahluwaliapranay727@gmail.com>
2025-06-12 19:29:20 +0530

    Enabled dnscrypt-proxy for DoH, updated system

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -1,8 +1,34 @@
 {
   networking = {
     hostName = "cypher";
-    networkmanager.enable = true;
+
+    nameservers = [ "127.0.0.1" "::1" ];
+    networkmanager = {
+      enable = true;
+      dns = "none";
+    };
+
     firewall.enable = false;
+  };
+
+  services.dnscrypt-proxy2 = {
+    enable = true;
+    settings = {
+      ipv6_servers = true;
+      require_dnssec = true;
+
+      sources.public-resolvers = {
+        urls = [
+          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+        ];
+        cache_file = "/var/cache/dnscrypt-proxy/public-resolvers.md";
+        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+      };
+
+      server_names = [ "cloudflare-family" "cloudflare-family-ipv6" ];
+
+    };
   };
 
   hardware.bluetooth = {
